eec09f3b7072f0dcbd82049090037edbc97e9af0
Thanh-Phong <hotv@clarkson.edu>
2016-04-13 06:47:29 -0400

    Changes for Android API level

--- app/src/main/java/com/thanh_phong/fsaedatalogger/MainActivity.java
+++ app/src/main/java/com/thanh_phong/fsaedatalogger/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
